128296e47fa3a4e177b20799d3861492b666f3a5
amashita <alterferow@gmail.com>
2016-12-11 13:02:05 +0900

    #1

---
+++ apps/BitTestTube.Core/ViewModels/TestTubePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Reactive.Bindings;
+
+namespace BitTestTube.ViewModels
+{
+    public class TestTubePageViewModel : BindableBase
+    {
+        public ReactiveProperty<string> Address { get; } = new ReactiveProperty<string>();
+
+        public ReactiveCommand NextCommand { get; }
+
+        public TestTubePageViewModel(INavigationService navigationService)
+        {
+        }
+    }
+}
